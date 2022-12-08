bd00e331b4ec255673a222147f56911c82006f3d
tsinatshimels <happyshimels65@gmail.com>
2022-12-08 15:33:28 -0800

    task 4 - 8

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("size of a char: %zu byte(s)\n", size of (char)) ;
+printf("size of an int: %zu byte(s)\n", size of (int)) ;
+printf("size of a long int: %zu byte(s)\n", size of (long int)) ;
+printf("size of a long long int: %zu byte(s)\n", size of (long long int)) ;
+printf("size of a float: %zu byte(s)\n", size of (float)) ;
+return (0) ;
+}
