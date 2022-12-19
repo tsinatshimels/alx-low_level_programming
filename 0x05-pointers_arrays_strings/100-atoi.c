ca734955cb21a358506dc94f123374f004aaf96b
tsinatshimels <happyshimels65@gmail.com>
2022-12-19 05:57:24 -0800

    task 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,7 +16,7 @@ int _atoi(char *s)
 		if (*(s + count) == '-')
 			pn *= -1;
 
-		if ((*(s + count) >= '0') && (*(s + count <= '9'))
+		if ((*(s + count) >= '0') && (*(s + count <= '9')))
 		{
 			if (size > 0)
 				n *= 10;
