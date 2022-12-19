4cfd6a9fe72c9dc4e02e5a18f0191f2f0ccc691a
tsinatshimels <happyshimels65@gmail.com>
2022-12-19 13:58:45 -0800

    task 100 update

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,7 +11,7 @@ int _atoi(char *s)
 	unsigned int num;
 	char *temp;
 
-	temp= s;
+	temp = s;
 	num = 0;
 	sign = 1;
 	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
@@ -25,7 +25,7 @@ int _atoi(char *s)
 		do {
 			num = num * 10 + (*temp - '0');
 			temp++;
-		}while (*temp >= '0' && *temp <= '9');
+		} while (*temp >= '0' && *temp <= '9');
 	}
 	return (num * sign);
 }
