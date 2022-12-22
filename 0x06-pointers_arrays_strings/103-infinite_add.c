4f126ccd0d1ab9cd2393bf1e00a62a7af09387f5
tsinatshimels <happyshimels65@gmail.com>
2022-12-22 13:20:31 -0800

    the last

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -8,8 +8,10 @@
 char *rot13(char *s)
 {
 		int i, j;
+
 		char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 			char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
 		for (i = 0; s[i] != '\0'; i++)
 		{
 			for (j = 0; a[j] != '\0'; j++)
